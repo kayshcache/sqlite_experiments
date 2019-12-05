2aaf8dc6813d1591eb0fb2602f5aa515dd47b43c
Staff GVA <GVA@Sach-Australia.local>
2019-12-06 05:09:30 +1100

    some dabbling

---
+++ schema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE students(
+student_id INTEGER PRIMARY KEY AUTOINCREMENT,
+fname TEXT NOT NULL,
+lname TEXT NOT NULL,
+age INTEGER NOT NULL
+);
+CREATE TABLE sqlite_sequence(name,seq);
+classes
+classes
+classes   students
+1|Modern History|23|Some data you set
+2|Advanced Chemistry|2|Some data you set
+3|Commercial Cookery|53|Some data you set
+4|Software Design and Development|12|Some data you set
